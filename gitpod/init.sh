e94fcb260068b562ab9efca1ed3dc5d47d1ece2e
Henit Chobisa <chobisa.henit@gmail.com>
2023-09-08 17:35:32 +0000

    Initialized Project

--- gitpod/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#init db
-psql --command "CREATE USER postgres WITH SUPERUSER PASSWORD 'postgres';" 
-psql --command "CREATE ROLE postgres SUPERUSER LOGIN PASSWORD 'postgres';"
-
-#reset app 
-gp env PHX_APP=""
-eval $(gp env -e) 
