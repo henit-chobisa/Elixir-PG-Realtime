0becdf404f2a066fccab58a3c52baccb50a716fe
Henit Chobisa <chobisa.henit@gmail.com>
2023-09-18 11:07:42 +0000

    doc: root module

--- raw/apps/pglistener/lib/postgres_listener.ex
+++ raw/apps/pglistener/lib/postgres_listener.ex
@@ -1,3 +1,7 @@
 defmodule PostgresListener do
 
+  @moduledoc """
+  The following module is the root of the project. This project includes starting listener processer instead of being a direct entension. Hence the root module stays empty and processes are being taken care by the supervisor and the registry modules.
+  """
+
 end
