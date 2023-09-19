f73f03100373dc84c377cc34154513857754020f
Henit Chobisa <chobisa.henit@gmail.com>
2023-09-19 08:10:14 +0000

    chore: updated config module, with persisted configs with agent

--- raw/apps/pglistener/lib/postgres_listener/configs/configs.ex
+++ raw/apps/pglistener/lib/postgres_listener/configs/configs.ex
@@ -1,5 +1,30 @@
 defmodule PostgresListener.Configs.Root do
 
+  @moduledoc """
+  An agent that takes care of the configuration for the specific app, for more than one app there would be more than one agents taking care of them as well.
+  """
+
+  alias PostgresListener.Configs.Registry
+
   use Agent
+
+  @doc """
+  Takes the opts for an app instance and sets the config as a state to the agent, along with registering self as an agent to the registry module.
+  """
+  def start_link(opts) do
+    configs = opts |> Keyword.get(:configs)
+    app_name = Keyword.get(configs, :name)
+    name = Registry.set_name(:set_agent, __MODULE__, app_name)
+    Agent.start_link(&(configs), name: name)
+  end
+
+
+  @doc """
+  Returns the configuration for a particular app from the registry. The registry fetches the current agent responsible for the current app with the __MODULE__ and the appname and then returns the current state from it.
+  """
+  def get_configs(app_name, keys \\ []) when is_list(keys) do
+    configs = WalEx.Registry.get_state(:get_agent, __MODULE__, app_name)
+    if Enum.empty?(keys), do: configs, else: Keyword.take(configs, keys)
+  end
 
 end
--- raw/apps/pglistener/lib/postgres_listener/configs/registry.ex
+++ raw/apps/pglistener/lib/postgres_listener/configs/registry.ex
@@ -1,3 +1,39 @@
 defmodule PostgresListener.Configs.Registry do
 
+  @moduledoc """
+  Registry module keeps the track of all of our started process, such as supervisor, replication processes and events. That's helpful when we have multiple apps running the same module, for multiple apps we have to run identical processes and registry is important to contextualize the processes, hence we are using app name to segrigate the processes.
+  """
+
+  # An atom that let's us track our registry
+  @pg_registry :pg_registry
+
+  @doc """
+  Starts a global registry ( All Apps that listens to the PG_Listener ) for all of the processes together, that's why we need to check if the registry is already running, because there can be two supervisors for two different apps, but registry must be same.
+  """
+  def start_registry do
+    # Find out if our process registry is running, if yes return the process id that is running the registry else start new and return it.
+    case Process.whereis(@pg_registry) do
+      nil -> Registry.start_link(keys: :unique, name: @pg_registry)
+      pid -> { :ok, pid }
+    end
+  end
+
+  # Set the processes for apps. You can understand it like { module + app_name } is kind of a unique identifier, that start a module for one app only once.
+  def set_name(:set_agent, module, app_name), do: set_name(module, app_name)
+  def set_name(:set_supervisor, module, app_name), do: set_name(:via, module, app_name)
+  def set_name(:set_genserver, module, app_name), do: set_name(:via, module, app_name)
+
+
+  @doc """
+  Generic function used by the external api to set the process to the registry
+  """
+  defp set_name(module, app_name), do: {:via, Registry, {@pg_registry, {module, app_name}}}
+
+  @doc """
+  Gets an state from an agent that is responsible for fetching the process from the module & app_name.
+  """
+  def get_state(:get_agent, module, app_name) do
+    Agent.get({:via, Registry, {:walex_registry, {module, app_name}}}, & &1)
+  end
+
 end
