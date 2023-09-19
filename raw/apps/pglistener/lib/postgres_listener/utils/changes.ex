69bf23588fb4fce8be87c87d2c1a44e5252ed6c0
Henit Chobisa <chobisa.henit@gmail.com>
2023-09-19 10:12:36 +0000

    feat: added utility modules for events

---
+++ raw/apps/pglistener/lib/postgres_listener/utils/changes.ex
@@ -0,0 +1,40 @@
+
+require Protocol
+
+defmodule PostgresListener.Utils.Changes do
+
+  @moduledoc """
+  The Modules defines new types for the messages, such as transaction, new record, update record, delete record and truncate record, so that these can be added to JASON.Encoder Protocol to extend parsing of our messages to these types, without us doing anything.
+  """
+
+  defmodule(Transaction, do: defstruct([:changes, :commit_timestamp]))
+
+  defmodule(NewRecord,
+    do: defstruct([:type, :record, :schema, :table, :columns, :commit_timestamp])
+  )
+
+  defmodule(UpdatedRecord,
+    do:
+      defstruct([
+        :type,
+        :old_record,
+        :record,
+        :schema,
+        :table,
+        :columns,
+        :commit_timestamp
+      ])
+  )
+
+  defmodule(DeletedRecord,
+    do: defstruct([:type, :old_record, :schema, :table, :columns, :commit_timestamp])
+  )
+
+  defmodule(TruncatedRelation, do: defstruct([:type, :schema, :table, :commit_timestamp]))
+end
+
+Protocol.derive(Jason.Encoder, PostgresListener.Utils.Transaction)
+Protocol.derive(Jason.Encoder, PostgresListener.Utils.NewRecord)
+Protocol.derive(Jason.Encoder, PostgresListener.Utils.UpdatedRecord)
+Protocol.derive(Jason.Encoder, PostgresListener.Utils.DeletedRecord)
+Protocol.derive(Jason.Encoder, PostgresListener.Utils.TruncatedRelation)
