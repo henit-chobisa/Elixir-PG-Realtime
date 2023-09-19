41305397f742343aca12c5730e0611dd04247678
Henit Chobisa <chobisa.henit@gmail.com>
2023-09-19 09:55:28 +0000

    feat: added types utility for Replication Publisher

--- raw/apps/pglistener/lib/postgres_listener/replication/replication_publisher.ex
+++ raw/apps/pglistener/lib/postgres_listener/replication/replication_publisher.ex
@@ -1,3 +1,9 @@
 defmodule PostgresListener.Replication.ReplicationPublisher do
 
+  use GenServer
+
+  def start_link(args) do
+    GenServer.start_link(__MODULE__, args, name: __MODULE__)
+  end
+
 end
